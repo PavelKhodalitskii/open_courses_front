e979c077cf798f3c35c530a3ce31434dc8f7a2bc
pavel <dreamsobenatic00@mail.ru>
2025-04-16 15:46:44 +0800

    Добавлен форма аутентификации

--- open_courses_front/src/App.tsx
+++ open_courses_front/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import CourseCreator from '@/pages/CourseCreatorMainPage'
+import LoginPage from '@/pages/LoginPage'
 import NotFoundPage from '@/pages/NotFoundPage'
 
 import { Toaster } from "@/components/ui/sonner";
@@ -12,6 +13,7 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CourseCreator />} />
+        <Route path="/login/" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
---
+++ open_courses_front/src/components/LoginForm.tsx
@@ -0,0 +1,79 @@
+import {useForm} from 'react-hook-form'
+
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {Button} from "@/components/ui/button";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+function LoginForm() {
+  const {
+    register, 
+    handleSubmit, 
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>();
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log("Ответ сервера:", result);
+    } catch (error) {
+      console.error("Ошибка:", error);
+    }
+  };
+
+  return (
+    <div className='p-10 rounded-md border  border-gray-400 h-90 flex flex-col justify-center gap-5'>
+      <h1 >Войти в Ипсилон</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-start gap-5 w-70">
+        <div className='flex flex-col gap-2 w-full'>
+          <Label htmlFor="email">Почта</Label>
+          <Input 
+            type='email'  
+            id='email'
+            placeholder='Почта'
+            {...register("email", {required: "Поле почты обязательно"})}
+            className='w-full bg-gray-100'
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+          )}
+        </div>
+        <div className='flex flex-col gap-2 w-full'>
+          <Label htmlFor="password">Пароль</Label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="••••••••"
+            className='w-full bg-gray-100'
+            {...register("password", { required: "Пароль обязателен" })}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
+          )}
+        </div>
+        <Button type="submit" className="w-full bg-green-300" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <>
+              Отправка...
+            </>
+          ) : (
+            <>
+              Войти
+            </>
+          )}
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm
